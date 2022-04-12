38d12d9b490382e68639392e55326bab22d09095
suraj4560 <surajbillionare@gmail.com>
2022-04-12 17:10:55 +0545

    hackerrank qu

---
+++ 12to24.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<string>
+using namespace std;
+string timeconversion(string s){
+    string two=s.substr(0,2);
+    int value =stoi(two);
+    if (s[8]=='P' && two !="12"){
+        value =value +12;
+        two=to_string(value);
+        return two+s.substr(2,6);
+    }
+    if (s[8]=='A' && two!="12"){
+        return s.substr(0,8);
+    }
+    if (s[8]=='A'&& two =="12"){
+        two ="00";
+        return two + s.substr(2,6);
+    }
+    return s.substr(0,8);
+    
+}
+int main(){
+    string s="12:45:54PM";
+    // string two=s.substr(0,2);
+    // cout<<two;
+    // int value =stoi(two);
+    // cout<<value;
+    // cout<<endl;
+    // if (s[8]=='P'){
+    //     value =value +12;
+    //     two=to_string(value);
+    //     cout<<two<<s.substr(2,6);
+    // }
+    // if (s[8]=='A'& two!="12"){
+    //     cout<<s.substr(0,8);
+    // }
+    // if (s[8]=='A'&& two =="12"){
+    //     two ="00";
+    //     cout<<two<<s.substr(2,6);
+    // }
+    // cout<<endl;
+    
+    string ans=timeconversion(s);
+    cout<<ans;
+    
+return 0;
+}
